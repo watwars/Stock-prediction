1f3467285cba65c6885b9f7f1ebce50be9ac313d
muyuan2003 <70287030+muyuan2003@users.noreply.github.com>
2021-09-18 22:40:43 -0400

    Add files via upload

--- python-backend/beta_and_growth_prediction.py
+++ python-backend/beta_and_growth_prediction.py
@@ -1,59 +1,46 @@
+import numpy as np
 import pandas as pd
+from statsmodels.tsa.arima.model import ARIMA
 import pandas_datareader.data as reader
 import datetime as dt
 
-stock_list = ['GOOGL', 'AAPL', 'MNST', 'AMZN', 'FB', 'TPR', 'ODFL']
-
-def beta_calculation(user_input):
+def beta_calculation(stock_ticker_symbol):
+    stock_info = []
     end = dt.datetime.now()
     start = dt.date(end.year - 2, end.month, end.day)
-    market_index = ['^IXIC']
 
-    df = reader.get_data_yahoo(market_index, start, end)
-    weekly_returns_market = df.resample('W').ffill().pct_change()
+    market_index = ['^IXIC']
+    df_market = reader.get_data_yahoo(market_index, start, end)
+    weekly_returns_market = df_market.resample('W').ffill().pct_change()
     weekly_returns_market = weekly_returns_market.dropna()
     weekly_returns_market = weekly_returns_market['Adj Close']
     weekly_returns_market = weekly_returns_market.rename(columns={'^IXIC': 'Market Avg'})
     market_variance = weekly_returns_market.var()
     market_variance = market_variance.iloc[0]
 
-    beta = 0
-    for stock_ticker_symbol in user_input:
-        df = reader.get_data_yahoo([stock_ticker_symbol], start, end)
-        weekly_returns_stock = df.resample('W').ffill().pct_change()
-        weekly_returns_stock = weekly_returns_stock.dropna()
-        weekly_returns_stock = weekly_returns_stock['Adj Close']
-        covariance_table = pd.concat([weekly_returns_market, weekly_returns_stock], axis = 1)
-        covariance = covariance_table.cov()
-        covariance = covariance.loc['Market Avg', stock_ticker_symbol].round(7)
-        beta = covariance / market_variance
-        beta = beta.round(3)
-    return beta
+    df_stock = reader.get_data_yahoo([stock_ticker_symbol], start, end)
+    weekly_returns_stock = df_stock.resample('W').ffill().pct_change()
+    weekly_returns_stock = weekly_returns_stock.dropna()
+    weekly_returns_stock = weekly_returns_stock['Adj Close']
+    covariance_table = pd.concat([weekly_returns_market, weekly_returns_stock], axis = 1)
+    covariance = covariance_table.cov()
+    covariance = covariance.loc['Market Avg', stock_ticker_symbol].round(7)
+    beta = covariance / market_variance
+    beta = beta.round(3)
+    stock_info.append(beta)
 
+    daily_price_stock = df_stock.to_period('D')
+    daily_price_stock = daily_price_stock.dropna()
+    daily_price_stock = daily_price_stock['Adj Close']
+    daily_price_stock = daily_price_stock[str(stock_ticker_symbol)]
+    train_data = daily_price_stock.iloc[0:1095]
+    model = ARIMA(train_data, order=(1, 1, 0), seasonal_order=(0, 0, 0, 0))
+    fitted_model = model.fit()
+    prediction = fitted_model.predict(start=1096, end=1096)
+    prediction = prediction.iloc[0]
+    prediction = prediction.round(3)
+    stock_info.append(prediction)
 
-def growth_category(growth_pct):
-    if growth_pct > 0.05:
-        return 'Large Increase'
-    elif growth_pct > 0.01:
-        return 'Small Increase'
-    elif growth_pct > 0:
-        return 'No increase'
-    elif growth_pct > -0.01:
-        return 'Small Decrease'
-    else:
-        return 'Large Decrease'
+    return stock_info
 
-
-'''
-for stock_ticker_symbol in user_input:
-    stock = []
-    stock.append(stock_ticker_symbol)
-    df = reader.get_data_yahoo(user_input, start, end)
-    weekly_returns_stock_ml = df.resample('W').ffill().pct_change()
-    weekly_returns_stock_ml = weekly_returns_stock_ml.dropna()
-    weekly_returns_stock_ml = weekly_returns_stock_ml['Adj Close']
-    print(weekly_returns_stock_ml)
-    #weekly_returns_stock_ml['Stock Growth Category'] = weekly_returns_stock_ml['Adj Close'].apply(growth_category)
-    #print(weekly_returns_stock_ml)
-'''
 
